d11dda9adc066b0dd70389f9a7838ec94079775f
tristan <tanshilinmail@gmail.com>
2020-10-21 17:18:00 +0800

    let_remote_mount_nfs.py

--- linux/ubuntu/let_remote_mount_nfs.py
+++ linux/ubuntu/let_remote_mount_nfs.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 target_server_list = []
 target_server_file_name = "target_server"
@@ -12,7 +13,11 @@ os.system("echo target_server: %s \n password:%s" % (str(target_server_list), pa
 
 # install sshpass
 os.system("apt-get install sshpass")
+time.sleep(1)
 
 os.system("echo send script file to target server")
 for item in target_server_list:
-    os.system("sshpass -p %s scp mount_nfs.py root@%s:mount_nfs.py" % (password, item))
+    item = item.strip()
+    command = """sshpass -p %s scp ~/mount_nfs.py root@%s:mount_nfs.py""" % (password, item)
+    os.system("""echo "command is: %s" """ % command)
+    os.system(command)
