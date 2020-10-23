e3123d3f0d50f84564590d7e904c035a69cbe949
tristan <tanshilinmail@gmail.com>
2020-10-23 18:35:49 +0800

    check_remote_lotus_worker.py let_remote_stop_start_lotus_worker.py stop_start_lotus_worker.sh

--- IPFS/lotus/check_remote_lotus_worker.py
+++ IPFS/lotus/check_remote_lotus_worker.py
@@ -29,12 +29,6 @@ os.system("apt-get install sshpass")
 time.sleep(1)
 
 
-def transfer_remote_file(remote_host, remote_password, local_filepath, remote_filepath):
-    command = """sshpass -p %s scp %s root@%s:%s""" % (remote_password, local_filepath, remote_host, remote_filepath)
-    os.system("""echo "command is: %s" """ % command)
-    os.system(command)
-
-
 def exec_remote_shell(remote_host_ip, remote_host_password, shell):
     command = """ sshpass -p %s ssh root@%s "%s" """ % (remote_host_password, remote_host_ip, shell)
     os.system("""echo "command is: %s" """ % command)
@@ -42,7 +36,6 @@ def exec_remote_shell(remote_host_ip, remote_host_password, shell):
 
 
 for item in target_server_list:
-    transfer_remote_file(item, password, "stop_start_lotus_worker.sh", "stop_start_lotus_worker.sh")
-    exec_remote_shell(item, password, "sh stop_start_lotus_worker.sh " + item)
+    exec_remote_shell(item, password, "ps -ef | grep lotus-worker")
 
 os.system(""" echo "all right done, thank you for use this script" """)
--- IPFS/lotus/stop_start_lotus_worker.sh
+++ IPFS/lotus/stop_start_lotus_worker.sh
@@ -3,6 +3,6 @@ kill $(ps -ef | grep lotus-worker | grep -v grep | awk '{print $2}')
 sleep 3;
 echo | ps -ef | grep lotus-worker | grep -v grep | awk '{print $2}'
 echo "start lotus-worker"
-nohup lotus-worker run --listen %s:3456 --attach /mnt/md0 >~/worker.log 2>&1 &
+nohup lotus-worker run --listen $1:3456 --attach /mnt/md0 >~/worker.log 2>&1 &
 sleep 3;
 echo | ps -ef | grep lotus-worker | grep -v grep | awk '{print $2}'
